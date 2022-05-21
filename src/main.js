eab59ec55184d08f6ab3e7cff05f1165d88f0c58
stiven45ruiz <stiven45ruiz@gmail.com>
2022-05-20 21:28:46 -0500

    Migracion a axios

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    params:{
+        'api_key': API_KEY,
+    },
+});
+
+
+
 const API_URL = 'https://api.themoviedb.org/3';
 const API_URL_TRENDING = '/trending/movie/day';
 const API_URL_GENRE = '/genre/movie/list';
@@ -5,8 +18,7 @@ const BASE_URL_IMG = 'https://image.tmdb.org/t/p/w300';
 
 
 const getTrendingMoviesPreview = async() =>{
-    const res = await fetch(`${API_URL}${API_URL_TRENDING}?api_key=${API_KEY}`);
-    const data = await res.json();
+    const {data} = await api(`${API_URL_TRENDING}`);
 
     const movies = data.results;
     console.log(data, movies);
@@ -29,9 +41,8 @@ const getTrendingMoviesPreview = async() =>{
 }
 
 const getCategoriesPreview = async() =>{
-    const res = await fetch(`${API_URL}${API_URL_GENRE}?api_key=${API_KEY}`);
-    const data = await res.json();
-
+    const {data} = await api(`${API_URL_GENRE}`);
+    
     const categories = data.genres;
     console.log(data, categories);
 
